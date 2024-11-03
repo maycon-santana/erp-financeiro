573e06e7ab68ba2ed793ed5363c91a40d424897f
maycon-santana <115080830+maycon-santana@users.noreply.github.com>
2024-11-03 16:59:32 -0300

    Criando o projeto

---
+++ siscom-servidor/src/main/java/com/t2ti/siscom/SiscomServidorApplication.java
@@ -0,0 +1,13 @@
+package com.t2ti.siscom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscomServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscomServidorApplication.class, args);
+	}
+
+}
---
+++ siscom-servidor/src/test/java/com/t2ti/siscom/SiscomServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2ti.siscom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscomServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
